5fb2d0583bc2c7a486612b968a31aa472ca3095d
fangstyle <fangstyles@163.com>
2017-03-29 14:02:49 +0800

    增添doc文件打开

--- pages/doc/doc.js
+++ pages/doc/doc.js
@@ -6,7 +6,35 @@ Page({
   },
   //doc在线预览
   loadDoc: function() {
+    console.log("点击下载");
+   wx.downloadFile({
+  url: 'http://192.168.1.132:3000/img/bg/123.docx',
+  success: function (res) {
+    console.log(res);
+    var filePath = res.tempFilePath;
+        console.log("下载成功"+filePath);
 
+   /*   wx.saveFile({
+      tempFilePath: filePath,
+      success: function(res) {
+        var savedFilePath = res.savedFilePath;
+        console.log("存储成功："+savedFilePath);
+      }
+    })
+    */
+
+        wx.openDocument({
+          filePath: filePath,
+          success: function (res) {
+          console.log('打开文档成功')
+      },fail:function(err){
+        for(var i in err ){
+          console.log(err[i]);
+        }
+      }
+    })
+  }
+})
   },
   //pdf在线预览
   loadPdf: function () {
