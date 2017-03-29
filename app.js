f14e4c7b8ad35508cafe1ae6212b47a99d27b553
fangstyle <fangstyles@163.com>
2017-03-29 18:11:56 +0800

    登陆基本完成

--- app.js
+++ app.js
@@ -4,7 +4,19 @@ App({
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    wx.setStorageSync('logs', logs);
+    if(this.globalData['3rd_session']){
+        //this.checkState();
+        wx.navigateTo({
+            url: 'pages/doc/doc'
+        })
+
+    }else{
+        //this.login();
+        wx.navigateTo({
+            url: 'pages/login/login'
+        });
+    }
   },
   getUserInfo:function(cb){
     var that = this
@@ -28,7 +40,46 @@ App({
       })
     }
   },
+   login:function () {
+       //调用登录接口
+       var self = this;
+       wx.login({
+           success: function(res) {
+               if (res.code) {
+                   //发起网络请求
+                   wx.request({
+                       url: 'https://test.com/onLogin',
+                       data: {
+                           code: res.code
+                       },
+                       success: function(response) {
+                           wx.setStorage({
+                               key:"3rd_session",
+                               data:response.vaule
+                           })
+                       }
+                   });
+
+               } else {
+                   console.log('获取用户登录态失败！' + res.errMsg)
+               }
+           }
+       })
+   },
+    checkState:function () {
+        wx.checkSession({
+            success: function(){
+                //session 未过期，并且在本生命周期一直有效 跳转首页
+
+            },
+            fail: function(){
+                //登录态过期
+                login(); //重新登录
+            }
+        })
+    },
   globalData:{
-    userInfo:null
+    userInfo:null,
+    "3rd_session":null
   }
 })
--- pages/doc/doc.js
+++ pages/doc/doc.js
@@ -2,70 +2,79 @@
 //获取应用实例
 var app = getApp()
 Page({
-  data: {
-  },
-  //doc在线预览
-  loadDoc: function() {
-    console.log("点击下载");
-   wx.downloadFile({
-  url: 'http://192.168.1.132:3000/img/bg/123.docx',
-  success: function (res) {
-    console.log(res);
-    var filePath = res.tempFilePath;
-        console.log("下载成功"+filePath);
+    data: {},
+    //doc在线预览
+    loadDoc: function () {
+        console.log("点击下载");
+        wx.downloadFile({
+            url: 'http://192.168.1.132:3000/img/bg/123.docx',
+            success: function (res) {
+                console.log(res);
+                var filePath = res.tempFilePath;
+                for(var j in res){
+                    console.log(res[j]);
+                }
+                //console.log("下载成功" + res);
 
-   /*   wx.saveFile({
-      tempFilePath: filePath,
-      success: function(res) {
-        var savedFilePath = res.savedFilePath;
-        console.log("存储成功："+savedFilePath);
-      }
-    })
-    */
+                /*   wx.saveFile({
+                 tempFilePath: filePath,
+                 success: function(res) {
+                 var savedFilePath = res.savedFilePath;
+                 console.log("存储成功："+savedFilePath);
+                 }
+                 })
+                 */
 
-        wx.openDocument({
-          filePath: filePath,
-          success: function (res) {
-          console.log('打开文档成功')
-      },fail:function(err){
-        for(var i in err ){
-          console.log(err[i]);
-        }
-      }
-    })
-  }
-})
-  },
-  //pdf在线预览
-  loadPdf: function () {
-    console.log("点击下载");
-   wx.downloadFile({
-  url: 'http://192.168.1.132:3000/img/bg/123.pdf',
-  success: function (res) {
-    console.log(res);
-    var filePath = res.tempFilePath;
-        console.log("下载成功"+filePath);
+                /*        wx.openDocument({
+                 filePath: filePath,
+                 success: function (res) {
+                 console.log('打开文档成功')
+                 },fail:function(err){
+                 for(var i in err ){
+                 console.log(err[i]);
+                 }
+                 }
+                 })*/
+            }
+        })
+    },
+    //pdf在线预览
+    loadPdf: function () {
+        console.log("点击下载");
+        wx.downloadFile({
+            url: 'http://192.168.1.132:3000/img/bg/123.pdf',
+            success: function (res) {
+                console.log(res);
+                var filePath = res.tempFilePath;
+                console.log("下载成功" + filePath);
 
-   /*   wx.saveFile({
-      tempFilePath: filePath,
-      success: function(res) {
-        var savedFilePath = res.savedFilePath;
-        console.log("存储成功："+savedFilePath);
-      }
-    })
-    */
+                /*   wx.saveFile({
+                 tempFilePath: filePath,
+                 success: function(res) {
+                 var savedFilePath = res.savedFilePath;
+                 console.log("存储成功："+savedFilePath);
+                 }
+                 })
+                 */
 
-        wx.openDocument({
-          filePath: filePath,
-          success: function (res) {
-          console.log('打开文档成功')
-      },fail:function(err){
-        for(var i in err ){
-          console.log(err[i]);
-        }
-      }
-    })
-  }
-})
-  }
+                wx.openDocument({
+                    filePath: filePath,
+                    success: function (res) {
+                        console.log('打开文档成功')
+                    }, fail: function (err) {
+                        for (var i in err) {
+                            console.log(err[i]);
+                        }
+                    }
+                })
+            }
+        })
+    },
+    getList: function () {
+        wx.getSavedFileList({
+            success: function (res) {
+                console.log(res.fileList)
+            }
+        })
+    }
 })
