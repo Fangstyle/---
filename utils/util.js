b1e3d19e703deab31d05d8f1e46918bd66c59ceb
fangstyle <fangstyles@163.com>
2017-03-31 11:06:47 +0800

    完善utils

--- utils/util.js
+++ utils/util.js
@@ -15,7 +15,31 @@ function formatNumber(n) {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
-
+function  httpGet(url,callback) {
+    wx.request({
+        url: url, //仅为示例，并非真实的接口地址
+        header: {
+            'content-type': ' '/**/
+        },
+        success: function(res) {
+            callback(res);
+        }
+    })
+}
+function httpPost(url,postparams,callback) {
+    wx.request({
+        url: url, //仅为示例，并非真实的接口地址
+        data: postparams,
+        header: {
+            'content-type': ' '/**/
+        },
+        success: function(res) {
+            callback(res);
+        }
+    })
+}
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  httpGet:httpGet,
+  httpPost:httpPost
 }
